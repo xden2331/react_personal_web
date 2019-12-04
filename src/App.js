37d68e37a93bb4650fc8fb61ef727e4de2e32566
RicXD <291261411@qq.com>
2019-12-04 13:54:09 +1100

    Complete Jumbotron

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {
+  Element,
+  scroller
+} from "react-scroll";
+
+import "./App.css";
+import "./stylesheets/home.css";
 
 function App() {
+  const scrollToElement = elementName => {
+    scroller.scrollTo(elementName, { smooth: true });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div id='top' className='jumbotron jumbotron-fluid'>
+        <div className='container'>
+          <h1>Richard Deng</h1>
+          <hr className='my-2' />
+          <p className='lead'>Interactive resume</p>
+        </div>
+
+        <div className='overlay'></div>
+
+        <div className='scroll-down d-none d-sm-block'>
+          <a onClick={() => scrollToElement("profile-section")}>
+            <span>
+              <i className='fas fa-arrow-down'></i>
+            </span>
+          </a>
+        </div>
+      </div>
+      <Element name='profile-section'>
+        <div className='background-vanilla-blue'>
+          <div className='container justify-content-center text-center ' id='profile'>
+            <h1>Richard Deng</h1>
+            <hr className='my-2' />
+            <p className='lead'>Interactive resume</p>
+          </div>
+        </div>
+      </Element>
     </div>
   );
 }
